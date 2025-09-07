ddb03b89113189ad60a643949b224d66c78d3413
Melissa Carvalho <ex195623@g.unicamp.br>
2025-09-07 15:21:53 -0300

    Fix produto bean

--- codigo_corrigir/src/br/unicamp/ic/inf335/beans/ProdutoBean.java
+++ codigo_corrigir/src/br/unicamp/ic/inf335/beans/ProdutoBean.java
@@ -63,12 +63,12 @@ public class ProdutoBean implements java.io.Serializable, Comparable<ProdutoBean
 		this.valor = valor;
 		this.estado = estado;
 	}
-	
+
 	@Override
 	public int compareTo(ProdutoBean p) {
 		if (valor > p.getValor()) {
 			return 1;
-		} else if (valor > p.getValor()) {
+		} else if (valor < p.getValor()) {
 			return -1;
 		} else
 		    return 0;
